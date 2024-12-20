50e7838d700aa6aca6dd2e7716aeecc84499e598
Rossyn <omaprakash2063@gmail.com>
2024-12-20 13:08:27 +0530

    Initial release of Toast library (v1.0.0)
    ### Added
    
    - Initial release of the library.
    - Basic implementation of Toast with flexible context and message length management.
    
    ### Notes
    
    - The library is ready for usage in Android apps with basic Toast functionality.

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## `[1.0.0] - 2024-12-20`
+
+### Added
+
+- Initial release of the library.
+- Basic implementation of Toast with flexible context and message length management.
+
+### Notes
+
+- The library is ready for usage in Android apps with basic Toast functionality.
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,17 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("OSSRH_USERNAME") ?: extra.properties["OSSRH_USERNAME"]?.toString() ?: "")
+            password.set(System.getenv("OSSRH_PASSWORD") ?: extra.properties["OSSRH_PASSWORD"]?.toString() ?: "")
+
+        }
+    }
 }
